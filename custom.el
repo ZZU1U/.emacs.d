f73c59d71cb37bac4d09b76be76ae11e0cb4d344
gleb <gl.anohin@yandex.ru>
2024-12-20 17:04:19 +0300

    Innnnit

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mac-command-modifier 'control)
+ '(mac-control-modifier 'super)
+ '(org-safe-remote-resources
+   '("\\`https://habrastorage\\.org/r/w1560/webt/b1/ru/kj/b1rukj6w-dgtyfncd3a62635zb0\\.png\\'"))
+ '(package-selected-packages '(org-roam-ui org-modern)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:height 1.5))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.75))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.25))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1)))))
